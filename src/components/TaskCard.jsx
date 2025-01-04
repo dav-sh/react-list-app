21e1836f69921fed334593729c93220ef03eeedb
David Orozco <dav.llwyr@gmail.com>
2025-01-04 00:14:24 -0600

    Tailwind added to the project

--- src/components/TaskCard.jsx
+++ src/components/TaskCard.jsx
@@ -2,21 +2,19 @@ import React from "react";
 import { useContext } from "react";
 import { TaskContext } from "../context/TaskContext";
 
-
-
 function TaskCard({ task }) {
-  const {deleteTask} = useContext(TaskContext)
+  const { deleteTask } = useContext(TaskContext);
   const actionButton = () => {
-   console.log('Hola')
-    deleteTask(task)
-  }
+    console.log("Hola");
+    deleteTask(task);
+  };
 
   return (
-    <>
+    <div className="bg-gray-800 text-white">
       <h1>{task.name}</h1>
       <p>{task.desc}</p>
       <button onClick={actionButton}>Eliminar tarea {task.id}</button>
-    </>
+    </div>
   );
 }
 
--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,8 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.jsx'
-import {TaskContextProvider} from './context/TaskContext.jsx'
+import App from './App'
+import {TaskContextProvider} from './context/TaskContext'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
